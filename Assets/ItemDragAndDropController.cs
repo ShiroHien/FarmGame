ced30f5d09bbaaf7706469f957666216d51dc17d
Shiro Hien <thaohien0607@gmail.com>
2023-08-09 15:05:42 +0700

    Visualize drag n drop item in inventory

--- Assets/ItemDragAndDropController.cs
+++ Assets/ItemDragAndDropController.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemDragAndDropController : MonoBehaviour
 {
     [SerializeField] ItemSlot itemSlot;
+    [SerializeField] GameObject itemIcon;
+    RectTransform iconTransform;
+    Image itemIconImage;
 
     private void Start() {
         itemSlot = new ItemSlot();
+        iconTransform = itemIcon.GetComponent<RectTransform>();
+        itemIconImage = itemIcon.GetComponent<Image>();
+    }
+
+    private void Update() {
+        if (itemIcon.activeInHierarchy == true) {
+            iconTransform.position = Input.mousePosition;
+        }
     }
 
     internal void OnClick(ItemSlot itemSlot) {
@@ -20,6 +32,17 @@ public class ItemDragAndDropController : MonoBehaviour
 
             itemSlot.Copy(this.itemSlot);
             this.itemSlot.Set(item, count);
+        }
+
+        UpdateIcon();
+    }
+
+    private void UpdateIcon() {
+        if (itemSlot.item == null) {
+            itemIcon.SetActive(false);
+        } else {
+            itemIcon.SetActive(true);
+            itemIconImage.sprite = itemSlot.item.icon;
         }
     }
 }
