bdb4484c521ac12419a25fa9c213982ed2507653
Shiro Hien <thaohien0607@gmail.com>
2023-08-11 16:36:32 +0700

    Add plowable land & interact

--- Assets/Scripts/ToolsCharacterController.cs
+++ Assets/Scripts/ToolsCharacterController.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class ToolsCharacterController : MonoBehaviour {
     CharacterController2D character;
     Rigidbody2D rgbd2d;
     [SerializeField] float offsetDistance = 1f;
     [SerializeField] float sizeOfInteractableArea = 1.2f;
+    [SerializeField] MarkerManager markerManager;
+    [SerializeField] TileMapReadController tileMapReadController;
+
+    Vector3Int selectedTilePosition;
+    bool selectable;
 
     private void Awake() {
         character = GetComponent<CharacterController2D>();
@@ -14,9 +21,19 @@ public class ToolsCharacterController : MonoBehaviour {
     }
 
     private void Update() {
+        SelectTile();
+        Marker();
         if (Input.GetMouseButtonDown(0)) { // left click
             UseTool();
         }
+    }
+
+    private void SelectTile() {
+        selectedTilePosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
+    }
+
+    private void Marker() {
+        markerManager.markedCellPosition = selectedTilePosition;
     }
 
     private void UseTool() {
