6e00ae099fe3664209a1fd7ff940c21dc2ff8370
Shiro Hien <thaohien0607@gmail.com>
2023-08-14 22:33:19 +0700

    Add a campfire

---
+++ Assets/Scripts/FireCamp/FireTriggerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTriggerController : MonoBehaviour
+{
+    bool playerinrange = false;
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (playerinrange == true && TemperatureController.currentTemperature < 100) {
+            TemperatureController.currentTemperature += 1;
+        }
+        if (!FindObjectOfType<SoundManager>().SoundIsPlaying("Fire")) {
+            FindObjectOfType<SoundManager>().Play("Fire");
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            playerinrange = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            playerinrange = false;
+        }
+    }
+}
